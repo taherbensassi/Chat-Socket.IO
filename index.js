c802ad6df7a47947a3c369c394ff0f8d79011de8
taherbensassi <taher.sassi.isamm@gmail.com>
2019-07-11 16:52:50 +0200

    add bootstrap and broadcast emit server side

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect',()=>{
+        console.log('a user disconnected');
+
+    })
+
+    socket.on('Created',(data)=>{
+        socket.broadcast.emit('Created', (data))
+    })
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
